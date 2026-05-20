ae4b4df6f3937e5b7db62bcd5ee6ad9b0a97e85b
klin521 <fiorentinamuno652@gmail.com>
2026-05-21 05:02:55 +0800

    Update route.ts

--- app/api/predictions/[id]/route.ts
+++ app/api/predictions/[id]/route.ts
@@ -1,26 +1,21 @@
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
-import { unstable_noStore as noStore } from "next/cache";
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 });
 
-// Prevent Next.js / Vercel from caching responses
-// See https://github.com/replicate/replicate-javascript/issues/136#issuecomment-1728053102
-replicate.fetch = (url, options) => {
-  return fetch(url, { cache: "no-store", ...options });
-};
-
-export async function GET(request: Request, { params }: any) {
-  noStore();
-  let prediction: any
-  // handle replicate / fal ... api
-
-  return NextResponse.json(
-    {
-      prediction,
-    },
-    { status: 200 }
-  );
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const prediction = await replicate.predictions.get(params.id);
+    return NextResponse.json(prediction, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json(
+      { detail: error.message },
+      { status: 500 }
+    );
+  }
 }
