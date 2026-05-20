9626b5f678c072f80f9ab8214510c50968339e2d
klin521 <fiorentinamuno652@gmail.com>
2026-05-21 03:08:12 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./i18n.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 强行关闭构建时的静态强迫症，全部走动态运行时
   typescript: {
-    ignoreBuildErrors: true, // 忽略代码类型检测的小报错
+    ignoreBuildErrors: true,
   },
   eslint: {
-    ignoreDuringBuilds: true, // 忽略代码格式检查
+    ignoreDuringBuilds: true,
   },
-  // 强制将所有包含动静态混合的页面，在打包时作为动态服务器渲染处理
   output: 'standalone'
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
